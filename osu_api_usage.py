62c5dabb0948974a63e48182504425a112ee578c
Altryd <michael_bor@mail.ru>
2024-06-08 17:51:18 +0400

    deleted comments

--- osu_api_usage.py
+++ osu_api_usage.py
@@ -116,32 +116,10 @@ def parse_scrim(match_id_arg=None, warmups=0, skip_last=0, verbose=True):
         match_id = match_id_arg if match_id_arg else 111534249
 
     scores_info, user_dict = parse_mplink(match_id, warmups, skip_last, verbose=False)
-    """with open("secrets.json", "r") as file:
-        secrets = json.loads(file.read())
-    token = requests.post("https://osu.ppy.sh/oauth/token",
-                          data="client_id={}&client_secret={}&grant_type=client_credentials&scope=public"
-                          .format(secrets["client_id"], secrets["client_secret"]),
-                          headers={"Accept": "application/json",
-                                   "Content-Type": "application/x-www-form-urlencoded"})  #
-    if token.status_code != 200:
-        print("Программа не смогла обратиться к API osu, проверьте интернет соединение или настройки json файла")
-        exit(-1)
-
-    access_token = token.json()["access_token"]
-
-    match_info_raw = requests.get("https://osu.ppy.sh/api/v2/matches/{}".format(match_id),
-                                  headers={"Authorization": "Bearer {}".format(access_token)})
-    if match_info_raw.status_code != 200:
-        print("Неверная ссылка на матч :( ")
-        exit(-1)
-    match_info_json = match_info_raw.json()
-    user_dict = dict()"""
     for user_id, user_info in user_dict.items():
         user_dict[user_id].update({"maps_won": 0})
-        # user_dict[user["id"]] = {"username": user["username"], "maps_won": 0}
 
     maps_played = len(scores_info)
-    # matchcost_etalon = 5 * (10**5)
     for score_info in scores_info:  # score: {"beatmap_id": ..., "scores": [...]}
         scores = score_info['scores']
         scores.sort(key=lambda x: x['score'], reverse=True)
@@ -150,16 +128,6 @@ def parse_scrim(match_id_arg=None, warmups=0, skip_last=0, verbose=True):
         player_won_map_id = scores[0]['user_id']
         user_dict[player_won_map_id]['maps_won'] += 1
 
-    """for event in match_info_json["events"]:
-        if event['detail']['type'] == 'other':
-            if len(event['game']['scores']) > 0:
-                maps_played += 1
-                sorted_scores = event['game']['scores']
-                sorted_scores.sort(key=lambda x: x['score'], reverse=True)
-                if len(sorted_scores) < 2:
-                    continue
-                player_won_map_id = sorted_scores[0]["user_id"]
-                user_dict[player_won_map_id]["maps_won"] += 1"""
     if verbose:
         print(user_dict)
         print("Maps played: {}".format(maps_played))
@@ -169,15 +137,6 @@ def parse_scrim(match_id_arg=None, warmups=0, skip_last=0, verbose=True):
     elif len(final_result) > 2:
         final_result = final_result[0:2]
     return json.dumps(final_result)
-    """
-    for user_id, user_dict_username in user_dict.items():
-        user_dict_username["average_score"] = user_dict_username["score_sum"] / maps_played
-    sorted_list_by_average = sorted(user_dict.items(), key=lambda item: item[1]["average_score"], reverse=True)
-    
-    for item in sorted_list_by_average:
-        print(f"avg.score: {item[1]['average_score']}, match_cost: {item[1]['average_score'] / matchcost_etalon},  "
-              f"score sum: { item[1]['score_sum']}  by {item[1]['username']}, user id: {item[0]}")
-    """
 
 
 def get_user_id_by_username(username):
@@ -200,7 +159,6 @@ def get_user_id_by_username(username):
         exit(-1)
 
     user_info = user_info_raw.json()
-    # print(user_info)
     return user_info['id']
 
 
