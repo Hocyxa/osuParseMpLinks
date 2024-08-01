8ece105ac7228329ef8c5f316e1699950d2bf6ce
altryd <michael_bor@mail.ru>
2024-08-01 22:13:08 +0400

    some fixes with setuptools, will see if that works

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='osuParseMpLinks',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(include=['osuParseMpLinks', 'osuParseMpLinks.*']),
     install_requires=[
         'Requests>=2.31.0'
-    ]
+    ],
+    include_package_data=True
 )
